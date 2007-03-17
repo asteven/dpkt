973c9c1acaa3c95983da34a1afdf2a9d96e27d2e
dugsong <dugsong@30ee8f51-df1a-0410-9115-614dbba9287e>
2007-03-17 03:33:16 +0000

    update based on http://wiki.wireshark.org/NullLoopback
    git-svn-id: http://dpkt.googlecode.com/svn/trunk@38 30ee8f51-df1a-0410-9115-614dbba9287e

--- dpkt/loopback.py
+++ dpkt/loopback.py
@@ -6,16 +6,15 @@ import dpkt, ethernet, ip, ip6
 
 class Loopback(dpkt.Packet):
     __hdr__ = (('family', 'I', 0), )
+    __byte_order__ = '@'
     def unpack(self, buf):
         dpkt.Packet.unpack(self, buf)
-        if self.family > 1500:
+        if self.family == 2:
+            self.data = ip.IP(self.data)
+        elif self.family == 0x02000000:
+            self.family = 2
+            self.data = ip.IP(self.data)
+        elif self.family in (24, 28, 30):
+            self.data = ip6.IP6(self.data)
+        elif self.family > 1500:
             self.data = ethernet.Ethernet(self.data)
-        else:
-            v = ord(self.data[0]) >> 4
-            if self.family == 2 or v == 4:
-                # AF_INET appears consistent
-                self.data = ip.IP(self.data)
-            elif v == 6:
-                # XXX - AF_INET6 differs on *BSD, MacOS X, etc.
-                self.data = ip6.IP6(self.data)
-
