d04799c15b98f1eb3a66dc4b24dc1b168b803938
jon.oberheide <jon.oberheide@30ee8f51-df1a-0410-9115-614dbba9287e>
2009-11-06 22:28:26 +0000

    ieee 802.11 and radiotap DLTs from timuralp@umich.edu
    git-svn-id: http://dpkt.googlecode.com/svn/trunk@56 30ee8f51-df1a-0410-9115-614dbba9287e

--- dpkt/pcap.py
+++ dpkt/pcap.py
@@ -11,32 +11,33 @@ PMUDPCT_MAGIC = 0xd4c3b2a1L
 PCAP_VERSION_MAJOR = 2
 PCAP_VERSION_MINOR = 4
 
-DLT_NULL =      0
-DLT_EN10MB =    1
-DLT_EN3MB =     2
-DLT_AX25 =      3
-DLT_PRONET =    4
-DLT_CHAOS =     5
-DLT_IEEE802 =   6
-DLT_ARCNET =    7
-DLT_SLIP =      8
-DLT_PPP =       9
-DLT_FDDI =      10
-# XXX - Linux
-DLT_LINUX_SLL = 113
-# XXX - OpenBSD
-DLT_PFLOG =     117
-DLT_PFSYNC =    18
+DLT_NULL               = 0
+DLT_EN10MB             = 1
+DLT_EN3MB              = 2
+DLT_AX25               = 3
+DLT_PRONET             = 4
+DLT_CHAOS              = 5
+DLT_IEEE802            = 6
+DLT_ARCNET             = 7
+DLT_SLIP               = 8
+DLT_PPP                = 9
+DLT_FDDI               = 10
+DLT_PFSYNC             = 18
+DLT_IEEE802_11         = 105
+DLT_LINUX_SLL          = 113
+DLT_PFLOG              = 117
+DLT_IEEE802_11_RADIO   = 127
+
 if sys.platform.find('openbsd') != -1:
-    DLT_LOOP =          12
-    DLT_RAW =           14
+    DLT_LOOP           = 12
+    DLT_RAW            = 14
 else:
-    DLT_LOOP =          108
-    DLT_RAW =           12
+    DLT_LOOP           = 108
+    DLT_RAW            = 12
 
 dltoff = { DLT_NULL:4, DLT_EN10MB:14, DLT_IEEE802:22, DLT_ARCNET:6,
            DLT_SLIP:16, DLT_PPP:4, DLT_FDDI:21, DLT_PFLOG:48, DLT_PFSYNC:4,
-           DLT_LOOP:4, DLT_RAW:0, DLT_LINUX_SLL:16 }
+           DLT_LOOP:4, DLT_LINUX_SLL:16 }
 
 class PktHdr(dpkt.Packet):
     """pcap packet header."""
@@ -101,8 +102,11 @@ class Reader(object):
             self.__ph = LEPktHdr
         elif self.__fh.magic != TCPDUMP_MAGIC:
             raise ValueError, 'invalid tcpdump header'
+        if self.__fh.linktype in dltoff:
+            self.dloff = dltoff[self.__fh.linktype]
+        else:
+            self.dloff = 0
         self.snaplen = self.__fh.snaplen
-        self.dloff = dltoff[self.__fh.linktype]
         self.filter = ''
 
     def fileno(self):
